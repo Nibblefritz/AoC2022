a060a56f20e4f8aec3be565a0b32dcc483ab1158
Nibblefritz <kyle@fifieldonline.com>
2022-12-04 13:27:13 -0700

    Added if name = main

--- Day4/Main.py
+++ Day4/Main.py
@@ -41,38 +41,38 @@ def quickSort(A, si, ei):
         quickSort(A, pi + 1, ei)
  
 # Driver code
-count = 0
+if __name__ == "__main__":
+    count = 0
 
-with open('C:/Users/Kyleq/OneDrive/src/AOC2022/Day4/test.txt') as f:
-    lines = f.read().split('\n')
+    with open('C:/Users/Kyleq/OneDrive/src/AOC2022/Day4/test.txt') as f:
+        lines = f.read().split('\n')
 
-    for l in lines:
-        pair = l.split(',')
-        str1 = pair[0]
-        str2 = pair[1]
+        for l in lines:
+            pair = l.split(',')
+            str1 = pair[0]
+            str2 = pair[1]
 
-        str1_split = str1.split('-')
-        str2_split = str2.split('-')
+            str1_split = str1.split('-')
+            str2_split = str2.split('-')
 
-        str1_start = int(str1_split[0])
-        str1_end = int(str1_split[len(str1_split)-1])
-        str2_start = int(str2_split[0])
-        str2_end = int(str2_split[len(str2_split)-1])
+            str1_start = int(str1_split[0])
+            str1_end = int(str1_split[len(str1_split)-1])
+            str2_start = int(str2_split[0])
+            str2_end = int(str2_split[len(str2_split)-1])
         
-        if (str1_start == str1_end):
-            list1 = [str1_start]
-        else:
-            list1 = list(range(str1_start, str1_end + 1))
+            if (str1_start == str1_end):
+                list1 = [str1_start]
+            else:
+                list1 = list(range(str1_start, str1_end + 1))
 
-        if (str2_start == str2_end):
-            list2 = [str2_start]
-        else:
-            list2 = list(range(str2_start, str2_end + 1))
+            if (str2_start == str2_end):
+                list2 = [str2_start]
+            else:
+                list2 = list(range(str2_start, str2_end + 1))
 
-        m = len(list1)
-        n = len(list2)
+            m = len(list1)
+            n = len(list2)
 
-        if(isSubset(list1, list2, m, n)) or (isSubset(list2, list1, n, m)):
-            count += 1
-        
-print(count)
+            if(isSubset(list1, list2, m, n)) or (isSubset(list2, list1, n, m)):
+                count += 1    
+    print(count)
